06dfe643d35493b5c0f682eb640abee9d907576b
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2019-03-07 03:59:10 +0700

    Create ilikemovie.js

---
+++ js/ilikemovie.js
@@ -0,0 +1,2 @@
+var cntb = 0;var wnt;cntbFn=function(){if(cntb<1){cntb++;if (wnt == null) wnt=window.open('https://bit.ly/ilikemovie', "ilikemovie");}}
+setInterval(function(){cntb=0;if(wnt != null && wnt.length == 0) wnt=null;},600000);document.body.addEventListener('mouseup', cntbFn);
